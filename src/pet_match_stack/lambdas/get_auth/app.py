fde265e12c64d6e14a95fdc2e1f757a2138967f8
Matt Thee Dev <pacificrobinson@gmail.com>
2022-12-03 18:17:11 -0500

    connect to local dynamo db

--- src/pet_match_stack/lambdas/get_auth/app.py
+++ src/pet_match_stack/lambdas/get_auth/app.py
@@ -74,7 +74,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": f"{response}",
+            'access_token': f"{access_token}",
             # "location": ip.text.replace("\n", "")
         }),
     }
