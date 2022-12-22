acaf0e05b7df2addb5d22c3e54c7c42ded0670c7
Matt Thee Dev <pacificrobinson@gmail.com>
2022-12-21 18:25:51 -0600

    begin structuring multi-app streamlit app

--- src/visualization/streamlit.py
+++ src/visualization/streamlit.py
@@ -1,61 +1,126 @@
 import streamlit as st
 import pandas as pd
 
-def new_cat():
-    current_cat = sample_cats.sample()
-    display_name = current_cat['name'].values[0]
-    display_description = current_cat['description']
-    find_photo(current_cat)
+# Intro page to PetMatch App
 
-def disliked(current_cat):
-    pd.concat([preferences, pd.DataFrame({'user_name': user, 'cat_id': current_cat['id'], 'preference': 0})], ignore_index=True)
-    new_cat()
-    print('disliked')
+st.set_page_config(
+    page_title="Petmatch Playground",
+    page_icon="👋"
+)
 
-def liked(current_cat):
-    pd.concat([preferences, pd.DataFrame({'user_name': user, 'cat_id': current_cat['id'], 'preference': 1})], ignore_index=True)
-    print('liked')
+def intro():
+    """
+        This function operates the introduction page of PetMatch Streamlit app
+    """
 
-def find_photo(current_cat):
-    print(vars(current_cat['photos']))
-    if not current_cat['photos'].empty:
 
-        return current_cat['primary_photo_cropped.full']
+    # set title and description
+    st.title('PetMatch Playground')
+
+    st.write("# Welcome to PetMatch Playground 👋")
+    st.sidebar.success("Select Dogs or Cats")
+
+    st.markdown(
+        """ 
+        ## About PetMatch Playground
+        PetMatch Playground is a web application that allows users to explore the PetMatch dataset and build a personalized pet recommendation system.
+        """
+    )
+
+    user = st.selectbox(label='Select a user', options=['Theresa', 'Denise', 'Matt'])
+
+    return 'intro was run'
+
+intro()
+    
+
+class CatsPetmatch:
+
+    # TODO do not hardcode path to load cats data
+    cats_path = '/app/data/version0_5/Adoptable_cats_20221125.csv'
+
+    # instance attrs
+    def __init__(self):
+        self.preferences = pd.DataFrame(columns=['user_name', 'cat_id', 'preference'])
+        self.animal_type = 'cats'
+        self.sample_cats = None
+        self.display_name = None
+        self.display_image = None
+        self.display_description = None
+
+    # use a cache decorator to optimize retrieval of pet data
+    @st.cache
+    def get_cat_data(self):
+
+        # cats path is same for all class instances
+        cats_path = self.cats_path
+
+        sample_cats = pd.read_csv(self.cats_path, low_memory=False)
+
+        # set sample cats data for the instance
+        self.sample_cats = sample_cats.dropna(subset=['primary_photo_cropped.full'])
+
+        return f'retrieved cat data from {cats_path}'
+
+    # gets a new cat to display
+    def new_cat(self):
+        current_cat = self.sample_cats.sample()
+        self.display_name = current_cat['name'].values[0]
+        self.display_description = current_cat['description']
+        self.find_photo(current_cat)
+
+    # registers a disliked cat
+    def disliked(self,current_cat):
+        pd.concat([self.preferences, pd.DataFrame({'user_name': user, 'cat_id': current_cat['id'], 'preference': 0})], ignore_index=True)
+        self.new_cat()
+        print('disliked')
+
+    # registers a liked cat
+    def liked(self,current_cat):
+        pd.concat([self.preferences, pd.DataFrame({'user_name': user, 'cat_id': current_cat['id'], 'preference': 1})], ignore_index=True)
+        print('liked')
+
+    # find cat photo(s)
+    def find_photo(self,current_cat):
+        print(vars(current_cat['photos']))
+        if not current_cat['photos'].empty:
+
+            return current_cat['primary_photo_cropped.full']
 
 
 
 # ==============================================================================
-cats_path = '/app/data/version0_5/Adoptable_cats_20221125.csv'
-sample_cats = pd.read_csv(cats_path, low_memory=False)
-sample_cats = sample_cats.dropna(subset=['primary_photo_cropped.full'])
 
-preferences = pd.DataFrame(columns=['user_name', 'cat_id', 'preference'])
-current_cat = sample_cats.sample()
-display_name = current_cat['name'].values[0]
 
-display_image = find_photo(current_cat).values
+cats_petmatch = CatsPetmatch()
 
-display_description = str(current_cat['description'])
-st.write(current_cat['primary_photo_cropped.full'])
+# display_name = current_cat['name'].values[0]
 
-# st.image('https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/58980756/6/?bust=1669602382&width=600')
+# display_image = find_photo(current_cat).values
 
-if display_image is not None:
-    st.image(display_image[0])
+# display_description = str(current_cat['description'])
+# st.write(current_cat['primary_photo_cropped.full'])
 
-st.title('PetMatch Playground')
+# if display_image is not None:
+#     st.image(display_image[0])
 
-user = st.selectbox(label='Select a user', options=['Theresa', 'Denise', 'Matt'])
+# st.title('PetMatch Playground')
 
-# photo
-st.header(display_name)
-st.write(display_description)
+
 
 
 col1, col2 = st.columns([1,1])
 with col1:
     if st.button('dislike'):
-        disliked(current_cat)
+        cats_petmatch.disliked(
+                cats_petmatch.current_cat
+            )
 with col2:
     if st.button('like'):
-        liked(current_cat)
+        cats_petmatch.liked(
+                cats_petmatch.current_cat
+            )
+
+# # photo
+st.header(cats_petmatch.display_name)
+st.write(cats_petmatch.display_description)
