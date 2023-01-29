496e7e6f619bdeaa1203929a61753279dcd08469
Matt Thee Dev <pacificrobinson@gmail.com>
2023-01-29 15:55:36 -0500

    add create dynamo table

--- src/pet_match_stack/db-setup/setup.sh
+++ src/pet_match_stack/db-setup/setup.sh
@@ -7,5 +7,7 @@ aws dynamodb create-table --cli-input-json file:///schemas/users-table-definitio
 
 aws dynamodb create-table --cli-inputjson file:///schemas/rankings-table-definition.json --endpoint-url "http://${dynamo}:8001"
 
+aws dynamodb create-table --cli-inputjson file:///schemas/user-feedback-table-definition.json --endpoint-url "http://${dynamo}:8001"
+
 # TODO
 # finish table setup with secondary indexes, etc
