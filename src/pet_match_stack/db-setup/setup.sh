60682a96fc66b9b43fdf953c37f767d627099593
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2023-01-29 16:15:35 -0600

    fix path

--- src/pet_match_stack/db-setup/setup.sh
+++ src/pet_match_stack/db-setup/setup.sh
@@ -3,11 +3,11 @@
 # This script is used to setup the dynamodb database for the pet_match_stack project.
 
 # create tables
-aws dynamodb create-table --cli-input-json file:///schemas/users-table-definition.json --endpoint-url "http://${dynamo}:8001"
+aws dynamodb create-table --cli-input-json file:///schemas/users-table-definition.json --endpoint-url "http://dynamo:8001"
 
-aws dynamodb create-table --cli-inputjson file:///schemas/rankings-table-definition.json --endpoint-url "http://${dynamo}:8001"
+aws dynamodb create-table --cli-inputjson file:///schemas/rankings-table-definition.json --endpoint-url "http://dynamo:8001"
 
-aws dynamodb create-table --cli-inputjson file:///schemas/user-feedback-table-definition.json --endpoint-url "http://${dynamo}:8001"
+aws dynamodb create-table --cli-inputjson file:///schemas/user-feedback-table-definition.json --endpoint-url "http://dynamo:8001"
 
 # TODO
 # finish table setup with secondary indexes, etc
