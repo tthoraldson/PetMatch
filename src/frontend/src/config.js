d911762f90ee6c4400f4ae6d69c55ca3d12706f1
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2023-02-03 16:52:25 -0600

    nEAT env

--- src/frontend/src/config.js
+++ src/frontend/src/config.js
@@ -1,21 +1,22 @@
-// @ts-ignore
-const configJson = require("./auth_config.json");
+var configJson = {};
+
+// TODO: This is horrid, deal with it
+if (process.env.REACT_APP_AUTH0_DOMAIN == null) {
+  // @ts-ignore
+  configJson = require("./auth_config.json");
+}
+
+
 
 export function getConfig() {
-  // Configure the audience here. By default, it will take whatever is in the config
-  // (specified by the `audience` key) unless it's the default value of "YOUR_API_IDENTIFIER" (which
-  // is what you get sometimes by using the Auth0 sample download tool from the quickstart page, if you
-  // don't have an API).
-  // If this resolves to `null`, the API page changes to show some helpful info about what to do
-  // with the audience.
-  const audience =
-    configJson.audience && configJson.audience !== "YOUR_API_IDENTIFIER"
-      ? configJson.audience
-      : null;
+  const audience = null;
+
+  const domain = process.env.REACT_APP_AUTH0_DOMAIN || configJson.domain;
+  const clientId = process.env.REACT_APP_AUTH0_CLIENTID || configJson.domain;
 
   return {
-    domain: configJson.domain,
-    clientId: configJson.clientId,
+    domain: domain,
+    clientId: clientId,
     ...(audience ? { audience } : null),
   };
 }
